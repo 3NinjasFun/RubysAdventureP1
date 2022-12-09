88357f372210f042c7ea43c581d9841de803e13b
3NinjasFun <739237@student.dusd.net>
2022-12-09 10:00:05 -0800

    Cog Bullet added

--- RubysAdventureSanchez/Assets/Scripts/RubysController.cs
+++ RubysAdventureSanchez/Assets/Scripts/RubysController.cs
@@ -7,19 +7,19 @@ public class RubysController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
+
+    public GameObject projectilePrefab;
 
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
-    public var projectilePrefab;
 
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
@@ -58,6 +58,11 @@ public class RubysController : MonoBehaviour
                 isInvincible = false;
         }
 
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
     }
 
     void FixedUpdate()
@@ -84,10 +89,10 @@ public class RubysController : MonoBehaviour
 
     void Launch()
     {
-    GameObject projectileObject = Instantiate(Projectile, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
-        Projectile projectile = projectileObject.getComponent<projectile>();
-        projecttile.Launch(lookDirection, 300);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
     }
